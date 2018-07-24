2576e55fe81d52665fcbe49d22543accbdb7daf2
delaney washington <delaneywashington123@gmail.com>
2018-07-24 15:26:41 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,3 +1,5 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
-
-
+def mothers_day (your_name)
+  puts "mothers_day#{your_name}"
+end
+mothers_day ("Belcalis Almanzar")
